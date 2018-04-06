c44ebc96d20b94aa5013691e9f1ae6eb4633e3db
Matt Nardoni <mattnardoni@MattNardoni.local>
2018-04-06 16:11:00 -0700

    Add html form and begin style

--- js/scripts.js
+++ js/scripts.js
@@ -1,36 +1,23 @@
-size = {
-  small: 6,
-  medium: 8,
-  large: 10
-};
-
-toppings = {
-  cheese: 1,
-  pepperoni: 1,
-  olives: 1,
-  sausage: 1,
-  mushroom: 1
-};
-
 function Pizza(name, toppings, size) {
   this.name = name;
   this.toppings = toppings;
   this.size = size;
-  this.pizzaOrder = function() {
-    return this.toppings + this.size;
-  };
 };
 
-
-
-
-
+Pizza.prototype.pizzaOrder = function() {
+  return this.toppings + this.size;
+};
 
 $(function() {
   $("#order").submit(function(event) {
     event.preventDefault();
     pizza1 = new Pizza($("input#pizza1").val());
     // pizza2 = new Pizza($("input#pizza1").val());
+
+    var orderInfo = pizza1.name + ", your order of one" + pizza1.size + "pizza with" + pizza1.toppings + "will be: ";
+        orderInfo += pizza1.pizzaOrder();
+    var order = $('#order1');
+    order.text = orderInfo;
 
 
   });
