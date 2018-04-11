072c9c2c19ee2383160a6078ec0e4fc14047f725
Matt Nardoni <mattnardoni@MattNardoni.netgear.com>
2018-04-10 23:08:35 -0700

    Array takes multiple toppings, value still undefined

--- js/scripts.js
+++ js/scripts.js
@@ -5,24 +5,23 @@ function Pizza(size) {
 }
 
 Pizza.prototype.pizzaOrder = function() {
-  for (var i = 0; i < pizza.topping.length; i++) {
+  for (var i = 0; i < pizza1.topping.length; i++) {
     this.cost = this.size += this.topping;
   }
+  return this.cost;
 };
 
 $(function() {
   $("#orderForm").submit(function(event) {
     event.preventDefault();
+    pizza1 = new Pizza();
     // var userToppings = parseInt($("input:checkbox[name=topping]:checked").val());
     $("input:checkbox[name=topping]:checked").each(function(){
-      userToppings.push($(this).val());
+      pizza1.topping.push($(this).val());
     });
-    console.log(userToppings)
     var userSizes = parseInt($("input:radio[name=size]:checked").val());
-
-
-    pizza1 = new Pizza (userToppings, userSizes);
-
+debugger;
+    console.log(pizza1);
     var orderInfo = "Your order will be: $";
         orderInfo += pizza1.pizzaOrder();
     var order = $('#orderConfirm');
